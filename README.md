77ad7cc88a5b0882e40d66a8db51119d2f1a0ded
freewind <nowindlee@gmail.com>
2018-06-25 12:34:45 +0800

    klaxon converters demo

--- README.md
+++ README.md
@@ -1,20 +1,6 @@
-Kotlin Hello World Demo
-=======================
+Kotlin Klaxon Demo
+==================
 
-Hello world from kotlin.
+Use Klaxon to parse and render JSON and objects.
 
-```
-./gradlew run
-```
-
-You will see "Hello, Kotlin!" from console.
-
-Run tests:
-
-```
-./gradlew test
-```
-
-You will see test result from console.
-
-
+Note the most important function is `wrapQuotes`.
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,12 +1,79 @@
 package example
 
-object Hello {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        println(hello("Kotlin"))
+import com.beust.klaxon.Converter
+import com.beust.klaxon.JsonValue
+import com.beust.klaxon.Klaxon
+
+fun main(args: Array<String>) {
+    parse()
+    render()
+}
+
+fun parse() {
+    val jsonString = """{"id" : "111", "side" : "buy", "symbol" : "ftusdt"}"""
+    val order = klaxon.parse<Order>(jsonString)
+    println(order)
+}
+
+fun render() {
+    val order = Order("111", Symbol("ft", "usdt"), OrderSide.Buy)
+    val json = klaxon.toJsonString(order)
+    println(json)
+}
+
+data class Order(
+        val id: String,
+        val symbol: Symbol,
+        val side: OrderSide
+)
+
+enum class OrderSide(val value: String) {
+    Buy("buy"), Sell("sell")
+}
+
+// IMPORTANT!!!!
+fun wrapQuotes(text: String) = "\"" + text + "\""
+
+val orderSideConverter = object : Converter {
+    override fun canConvert(cls: Class<*>): Boolean {
+        return cls == OrderSide::class.java
     }
 
-    fun hello(name: String): String = "Hello, $name!"
+    override fun fromJson(jv: JsonValue): OrderSide {
+        return OrderSide.values().find { it.value == jv.inside } ?: throw IllegalArgumentException("invalid json value: ${jv.inside}")
+    }
+
+    override fun toJson(value: Any): String {
+        return wrapQuotes((value as OrderSide).value)
+    }
+}
+
+val symbolConverter = object : Converter {
+    override fun canConvert(cls: Class<*>): Boolean {
+        return cls == Symbol::class.java
+    }
+
+    override fun fromJson(jv: JsonValue): Any {
+        val value = jv.inside as String
+        val quote = "usdt"
+        if (value.endsWith(quote)) {
+            return Symbol(value.removeSuffix("quote"), quote)
+        } else {
+            throw IllegalArgumentException("Invalid symbol: $value")
+        }
+    }
+
+    override fun toJson(value: Any): String {
+        return wrapQuotes((value as Symbol).toString())
+    }
 
 }
+
+data class Symbol(val base: String, val quote: String) {
+    override fun toString(): String {
+        return base + quote
+    }
+}
+
+val klaxon = Klaxon().converter(orderSideConverter).converter(symbolConverter)
 
